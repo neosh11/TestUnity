7fda6cd3ccaecaa16f6375cc4875220df25eb107
Neosh Sheikh <neosh11@gmail.com>
2021-02-27 13:41:15 +0800

    Added Movement and camera stuffs

---
+++ TestingUnity/Assets/KeyboardInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Script/Movement Input")]
+public class KeyboardInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed = 6.0f;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float deltaZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float deltaY = Input.GetAxis("UpDown") * speed * Time.deltaTime;
+        transform.Translate(deltaX , deltaY, deltaZ);
+    }
+}
---
+++ TestingUnity/Assets/MouseLook.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    // Sensitivities for mouse inputs
+    public float sensitivityHor = 9.0f;
+    public float sensitivityVert = 9.0f;
+
+
+    // Limits on vertical + mouse vert angle
+    private float _rotationX = 0;
+    public float minimumVert = -45.0f;
+    public float maximumVert = 45.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // GetAxis is rate independent
+        // Calc and clamp vertical rotation 
+        _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+        _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+        // Calc and pres horizontal rotaion
+        float delta = Input.GetAxis("Mouse X") * sensitivityHor;
+        float rotationY = transform.localEulerAngles.y + delta;
+
+        transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+
+
+    }
+}
